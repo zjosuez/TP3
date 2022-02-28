08ae7131355f566e43826c498f4bcca25f633746
zjosu <zjosuez@outlook.com>
2022-02-28 19:32:33 -0300

    Added Roupa e acessorio CRUD

--- projeto3/src/projeto3/Principal.java
+++ projeto3/src/projeto3/Principal.java
@@ -1,10 +1,19 @@
 package projeto3;
 
+import java.util.*;
+
 public class Principal {
-
+	static RoupaEAcessorio a1;
+	static RoupaEAcessorio a2;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		a1 = new RoupaEAcessorio("camisa","camisas", "camisa lisa", 18, "camisa muito linda", "bom estado", 1800, "não sei", "masculino");
+		a1.cadastrar();
+		a1.buscar("camisa");
+		
+//		a1.editar(0, "calca", "calca", "calca", 24, "calca", "calca", 2000, "calca", "calca");
+		a2 = new RoupaEAcessorio("calca","calcas", "calca lisa", 18, "calca muito linda", "bom estado", 1800, "não sei", "masculino");
+		
 	}
-
 }
--- projeto3/src/projeto3/RoupaEAcessorio.java
+++ projeto3/src/projeto3/RoupaEAcessorio.java
@@ -1,6 +1,9 @@
 package projeto3;
 
+import java.util.ArrayList;
+
 public class RoupaEAcessorio {
+	
 	private String nome;
 	private String categoria;
 	private String modelo;
@@ -10,11 +13,12 @@ public class RoupaEAcessorio {
 	private double preco;
 	private String tipo;
 	private String genero;
+	private ArrayList<RoupaEAcessorio> products = new ArrayList<RoupaEAcessorio>();
 	
-	// Constructor
+	// Constructor 
 	public RoupaEAcessorio(String nome, String categoria, String modelo, int tamanho, String descricao,
 			String condicaoDeUso, double preco, String tipo, String genero) {
-		super();
+	
 		this.nome = nome;
 		this.categoria = categoria;
 		this.modelo = modelo;
@@ -30,77 +34,122 @@ public class RoupaEAcessorio {
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getCategoria() {
-		return categoria;
+	
+	public void getCategoria() {
+		System.out.println(categoria);
 	}
+	
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-	public String getModelo() {
-		return modelo;
+	
+	public void getModelo() {
+		System.out.println(modelo);
 	}
+	
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-	public int getTamanho() {
-		return tamanho;
+	
+	public void getTamanho() {
+		System.out.println(tamanho);
 	}
+	
 	public void setTamanho(int tamanho) {
 		this.tamanho = tamanho;
 	}
-	public String getDescricao() {
-		return descricao;
+	
+	public void getDescricao() {
+		System.out.println(descricao);
 	}
+	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public String getCondicaoDeUso() {
-		return condicaoDeUso;
+	
+	public void getCondicaoDeUso() {
+		System.out.println(condicaoDeUso);
 	}
+	
 	public void setCondicaoDeUso(String condicaoDeUso) {
 		this.condicaoDeUso = condicaoDeUso;
 	}
-	public double getPreco() {
-		return preco;
+	
+	public void getPreco() {
+		System.out.println(preco);
 	}
+	
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-	public String getTipo() {
-		return tipo;
+	
+	public void getTipo() {
+		System.out.println(tipo);
 	}
+	
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	public String getGenero() {
-		return genero;
+	
+	public void getGenero() {
+		System.out.println(genero);
 	}
+	
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
 
 	// Methods
-	public void cadastrar() {
+	public void cadastrar() {     
+		products.add(new RoupaEAcessorio(nome, categoria, modelo, tamanho, descricao, condicaoDeUso, preco, tipo, genero));
+		System.out.println("O produto " + nome + " foi cadastrado com sucesso!");
 		
 	}
 	
-	public void editar() {
-		
+	public void editar(int posicaoEditar,String nome, String categoria, String modelo, int tamanho, String descricao, String condicaoDeUso, double preco,  String tipo, String genero) {
+		products.get(posicaoEditar).setNome(nome);
+		products.get(posicaoEditar).setCategoria(categoria);
+		products.get(posicaoEditar).setModelo(modelo);
+		products.get(posicaoEditar).setTamanho(tamanho);
+		products.get(posicaoEditar).setDescricao(descricao);
+		products.get(posicaoEditar).setCondicaoDeUso(condicaoDeUso);
+		products.get(posicaoEditar).setPreco(preco);
+		products.get(posicaoEditar).setTipo(tipo);
+		products.get(posicaoEditar).setGenero(genero);
 	}
 	
-	public void deletar() {
-		
+	public void deletar(int posicaoExcluir) {
+		System.out.println("O produto " + products.get(posicaoExcluir).getNome() + " foi removido com sucesso!");
+		products.remove(posicaoExcluir);
+			
 	}
 	
 	public void ler() {
-		
+		for(int i = 0; i < products.size(); i++) {
+			System.out.println(products.get(i));
+		}
 	}
 	
-	public void buscar() {
-		
+	@Override
+	public String toString() {
+		return "RoupaEAcessorio [nome=" + nome + ", categoria=" + categoria + ", modelo=" + modelo + ", tamanho="
+				+ tamanho + ", descricao=" + descricao + ", condicaoDeUso=" + condicaoDeUso + ", preco=" + preco
+				+ ", tipo=" + tipo + ", genero=" + genero + ", products=" + products + "]";
+	}
+
+	public void buscar(String nomeBusca) {
+		for(int i = 0; i < products.size() ; i++) {
+			if (products.get(i).getNome().equals(nomeBusca)) {
+				System.out.println("O produto " + products.get(i).getNome() + " foi encontrado");
+			} else {
+				System.out.println("Produto não encontrado");
+			}
+			
+		}
 	}
 	
 }
